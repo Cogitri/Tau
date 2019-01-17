1a14ccd31be9a6f4892c0b9c25b72a78976a6803
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2019-01-18 00:33:53 +0100

    fix(build): honor RUST_TARGET variable when copying binaries

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -2,4 +2,4 @@
 
 export GXI_PLUGIN_DIR=$5
 
-cargo build --manifest-path $1/Cargo.toml --release && cp $3/target/release/$4 $2
+cargo build --manifest-path $1/Cargo.toml --release && cp $3/target/${RUST_TARGET}/release/$4 $2
