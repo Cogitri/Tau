1d7ee7a286e264ef08f0a9aaac15e064716397c0
maxice8 <thinkabit.ukim@gmail.com>
2019-01-21 11:45:44 -0200

    remove rust-target option, just use $RUST_TARGET from environment

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-export GXI_PLUGIN_DIR=$6
+export GXI_PLUGIN_DIR=$5
 
-cargo build --manifest-path $1/Cargo.toml --target-dir $3 --release && cp $3/$5/release/$4 $2
+if [ "$6" = "true" ]; then
+	path="${3}/${RUST_TARGET}/release/${4}"
+else
+	path="${3}/release/${4}"
+fi
+
+cargo build --manifest-path $1/Cargo.toml --target-dir $3 --release && cp "$path" $2
