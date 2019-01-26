32ff954db8e8247994dc81af06103bdbabc57f2c
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2019-01-26 21:58:02 +0100

    fixup! chore(meson): fix compilation in flatpaks

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export GXI_PLUGIN_DIR=$6
-export GXI_LOCALEDIR=$7
+export GXI_PLUGIN_DIR=$7
+export GXI_LOCALEDIR=$8
 export GXI_APP_ID="com.github.Cogitri.gxi"
-export GXI_VERSION=$8
+export GXI_VERSION=$9
 
 cd $1
 cargo build --target-dir $4 --release && cp $4/$6/release/$5 $2/$3
