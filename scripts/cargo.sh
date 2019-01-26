0ac7f60649769d57aabffe1e42fd60b64d2609d7
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2019-01-26 22:05:06 +0100

    feat(cargo): print variables during build

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -5,5 +5,15 @@ export GXI_LOCALEDIR=$8
 export GXI_APP_ID="com.github.Cogitri.gxi"
 export GXI_VERSION=$9
 
+export GREEN='\033[0;32m'
+export NO_COLOR='\033[0m'
+
+echo "\tGXI Plugindir: ${GREEN}${GXI_PLUGIN_DIR}${NO_COLOR}
+\tGXI Localedir: ${GREEN}${GXI_LOCALEDIR}${NO_COLOR}
+\tGXI App-ID:    ${GREEN}${GXI_APP_ID}${NO_COLOR}
+\tGXI Version:   ${GREEN}${GXI_VERSION}${NO_COLOR}
+"
+
+
 cd $1
 cargo build --target-dir $4 --release && cp $4/$6/release/$5 $2/$3
