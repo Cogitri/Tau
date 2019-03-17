eae9c7ac703a66b3fb5725bfd95d92e1ffce66ca
Rasmus Thomsen <oss@cogitri.dev>
2019-03-17 16:20:25 +0100

    chore(meson): rewrite meson_post_install in SH

---
+++ meson_post_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$DESTDIR" ]; then
+    schemadir="$MESON_INSTALL_PREFIX/share/glib-2.0/schemas"
+    icondir="$MESON_INSTALL_PREFIX/share/icons/hicolor"
+
+    printf "%s\\n" "Compiling GSettings schemas in dir $schemadir"
+    glib-compile-schemas $schemadir
+
+    printf "%s\\n" "Updating GTK icon cache in dir $icondir"
+    gtk-update-icon-cache -f -t $icondir
+
+    printf "%s\\n" "Updating desktop (MIME) database"
+    update-desktop-database
+fi
