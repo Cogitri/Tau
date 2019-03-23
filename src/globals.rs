bc9e06054ca0db04c8972c49bcad8348cf786a9e
Rasmus Thomsen <oss@cogitri.dev>
2019-03-23 10:30:26 +0100

    refactor(globals): add PLUGIN_DIR static instead of hacking around with build.rs

--- src/globals.rs
+++ src/globals.rs
@@ -1,3 +1,4 @@
 pub static LOCALEDIR: Option<&'static str> = option_env!("GXI_LOCALEDIR");
 pub static APP_ID: &str = "com.github.Cogitri.gxi";
 pub static VERSION: Option<&str> = option_env!("GXI_VERSION");
+pub static PLUGIN_DIR: Option<&str> = option_env!("GXI_PLUGIN_DIR");
