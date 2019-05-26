96ca9aef507fb6d9f76b76dd78831e10cd21d2b0
Rasmus Thomsen <oss@cogitri.dev>
2019-05-26 15:30:02 +0200

    refactor(editview): split up into more modules
    The edit_view.rs files has become pretty huge, so it makes sense to split off
    the GTK Widget stuff into view_item.rs and the font related stuff into fonts.rs

--- src/editview/src/lib.rs
+++ src/editview/src/lib.rs
@@ -2,8 +2,10 @@
 extern crate enclose;
 
 pub mod edit_view;
+pub mod fonts;
 pub mod main_state;
 pub mod theme;
+mod view_item;
 
 pub use crate::edit_view::EditView;
 pub use crate::main_state::{MainState, Settings};
