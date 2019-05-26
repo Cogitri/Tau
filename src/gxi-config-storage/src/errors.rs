177315c780c2048603412a9e37534b7b01cde0a4
Rasmus Thomsen <oss@cogitri.dev>
2019-05-26 15:12:35 +0200

    feat(gxi-config-storage): add try_* functions for panic-safe access to GSettings

--- src/gxi-config-storage/src/errors.rs
+++ src/gxi-config-storage/src/errors.rs
@@ -2,6 +2,16 @@ use failure::Fail;
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Fail)]
 pub enum Error {
-    #[fail(display = "Failed to set GSettings key! Error: {}", _0)]
-    GSettings(String),
+    #[fail(display = "Failed to set readonly key '{}'", _0)]
+    ReadOnly(String),
+    #[fail(display = "Tried to get non-existent key '{}'!", _0)]
+    GetNonExistent(String),
+    #[fail(display = "Tried to set non-existent key '{}'!", _0)]
+    SetNonExistent(String),
+    #[fail(display = "Couldn't retrieve schema source!")]
+    NoSchemaSource,
+    #[fail(display = "Couldn't get String for key '{}'", _0)]
+    NoString(String),
+    #[fail(display = "Couldn't get Variant for key '{}'", _0)]
+    NoValue(String),
 }
--- src/gxi-config-storage/src/pref_storage.rs
+++ src/gxi-config-storage/src/pref_storage.rs
@@ -1,11 +1,30 @@
 use crate::errors::Error;
-use gio::{Settings, SettingsExt};
+use gio::{Settings, SettingsExt, SettingsSchemaSource};
 use glib::Variant;
 
+/// Trait to use `GSchema::get_key` and `GSchema::set_key` for any supported type.
 pub trait GSchemaExt<RHS = Self> {
-    fn get_key(&self, field_name: &str) -> RHS;
+    /// Get a key from the GSchema
+    ///
+    /// # Panics
+    ///
+    /// Panics if the key specified by `key_name` isn't present in the GSchema
+    /// See `GSchema::try_get_key` for a panic-free variant.
+    fn get_key(&self, key_name: &str) -> RHS;
 
-    fn set_key(&self, field_name: &str, val: RHS) -> Result<(), Error>;
+    /// Set a key in the Gschema
+    ///
+    /// # Panics
+    ///
+    /// Panics if the key specified by `key_name` isn't present in the GSchema.
+    /// See `GSchema::try_set_key` for a panic-free variant.
+    fn set_key(&self, key_name: &str, val: RHS) -> Result<(), Error>;
+
+    /// Get a key from the GSchema, panic free.
+    fn try_get_key(&self, key_name: &str) -> Result<RHS, Error>;
+
+    /// Set a key in the Gschema, panic free.
+    fn try_set_key(&self, key_name: &str, val: RHS) -> Result<(), Error>;
 }
 
 #[derive(Clone)]
@@ -18,12 +37,25 @@ impl GSchema {
     ///
     /// # Panics
     ///
-    /// This panics if it can't find the GSchema, e.g. because it
-    /// hasn't been installed correctly.
+    /// Panics if it can't find the GSchema, e.g. because it
+    /// hasn't been installed correctly. See `GSchema::try_new` for a
+    /// panic safe version
     pub fn new(schema_name: &str) -> Self {
         Self {
             settings: Settings::new(schema_name),
         }
+    }
+
+    /// The panic-safe version of `GSchema::new`. Instead of simply requiring the
+    /// requested GSchema to be present it first looks up if it exists and then
+    /// creates it. Do note that you have to use `Gschema::try_get_key` and
+    /// `GSchema::try_set_key` to also set keys panic-free
+    pub fn try_new(schema_name: &str) -> Option<Self> {
+        SettingsSchemaSource::get_default()
+            .and_then(|settings_source| settings_source.lookup(schema_name, true))
+            .map(|_| Self {
+                settings: Settings::new(schema_name),
+            })
     }
 }
 
@@ -38,10 +70,41 @@ impl GSchemaExt<String> for GSchema {
         if res {
             Ok(())
         } else {
-            Err(Error::GSettings(format!(
-                "Key {} isn't writeable!",
-                key_name
-            )))
+            Err(Error::ReadOnly(key_name.to_string()))
+        }
+    }
+
+    fn try_get_key(&self, key_name: &str) -> Result<String, Error> {
+        if let Some(schema_source) = self.settings.get_property_settings_schema() {
+            if schema_source.has_key(key_name) {
+                if let Some(val) = self.settings.get_string(key_name) {
+                    Ok(val.to_string())
+                } else {
+                    Err(Error::NoString(key_name.to_string()))
+                }
+            } else {
+                Err(Error::GetNonExistent(key_name.to_string()))
+            }
+        } else {
+            Err(Error::NoSchemaSource)
+        }
+    }
+
+    fn try_set_key(&self, key_name: &str, val: String) -> Result<(), Error> {
+        if let Some(schema_source) = self.settings.get_property_settings_schema() {
+            if schema_source.has_key(key_name) {
+                let res = self.settings.set_string(key_name, &val);
+
+                if res {
+                    Ok(())
+                } else {
+                    Err(Error::ReadOnly(key_name.to_string()))
+                }
+            } else {
+                Err(Error::SetNonExistent(key_name.to_string()))
+            }
+        } else {
+            Err(Error::NoSchemaSource)
         }
     }
 }
@@ -57,10 +120,41 @@ impl GSchemaExt<Variant> for GSchema {
         if res {
             Ok(())
         } else {
-            Err(Error::GSettings(format!(
-                "Key {} isn't writeable!",
-                key_name
-            )))
+            Err(Error::ReadOnly(key_name.to_string()))
+        }
+    }
+
+    fn try_get_key(&self, key_name: &str) -> Result<Variant, Error> {
+        if let Some(schema_source) = self.settings.get_property_settings_schema() {
+            if schema_source.has_key(key_name) {
+                if let Some(val) = self.settings.get_value(key_name) {
+                    Ok(val)
+                } else {
+                    Err(Error::NoValue(key_name.to_string()))
+                }
+            } else {
+                Err(Error::GetNonExistent(key_name.to_string()))
+            }
+        } else {
+            Err(Error::NoSchemaSource)
+        }
+    }
+
+    fn try_set_key(&self, key_name: &str, val: Variant) -> Result<(), Error> {
+        if let Some(schema_source) = self.settings.get_property_settings_schema() {
+            if schema_source.has_key(key_name) {
+                let res = self.settings.set_value(key_name, &val);
+
+                if res {
+                    Ok(())
+                } else {
+                    Err(Error::ReadOnly(key_name.to_string()))
+                }
+            } else {
+                Err(Error::SetNonExistent(key_name.to_string()))
+            }
+        } else {
+            Err(Error::NoSchemaSource)
         }
     }
 }
