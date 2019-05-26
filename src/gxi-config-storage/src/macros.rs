52826171b7ed44a6ee44b9932b68fa82036e8e5c
Rasmus Thomsen <oss@cogitri.dev>
2019-05-26 15:12:35 +0200

    feat(gxi-config-storage): add try_* functions for panic-safe access to GSettings

--- src/gxi-config-storage/src/macros.rs
+++ src/gxi-config-storage/src/macros.rs
@@ -11,10 +11,36 @@ macro_rules! impl_typed_getset {
                 if res {
                     Ok(())
                 } else {
-                    Err(Error::GSettings(format!(
-                        "Key {} isn't writeable!",
-                        key_name
-                    )))
+                    Err(Error::ReadOnly(key_name.to_string()))
+                }
+            }
+            fn try_get_key(&self, key_name: &str) -> Result<$ty, Error> {
+                if let Some(schema_source) = self.settings.get_property_settings_schema() {
+                    if schema_source.has_key(key_name) {
+                        Ok(self.settings.$getter(key_name))
+                    } else {
+                        Err(Error::GetNonExistent(key_name.to_string()))
+                    }
+                } else {
+                    Err(Error::NoSchemaSource)
+                }
+            }
+
+            fn try_set_key(&self, key_name: &str, val: $ty) -> Result<(), Error> {
+                if let Some(schema_source) = self.settings.get_property_settings_schema() {
+                    if schema_source.has_key(key_name) {
+                        let res = self.settings.$setter(key_name, val);
+
+                        if res {
+                            Ok(())
+                        } else {
+                            Err(Error::ReadOnly(key_name.to_string()))
+                        }
+                    } else {
+                        Err(Error::SetNonExistent(key_name.to_string()))
+                    }
+                } else {
+                    Err(Error::NoSchemaSource)
                 }
             }
         }
