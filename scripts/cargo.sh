cf3c681ac7f8976c4c208b7d99d3daf832ea21ef
Rasmus Thomsen <oss@cogitri.dev>
2019-06-04 15:26:23 +0200

    chore(meson): instead of a fragile check for crossbuilding just check which path exists

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
+set -e
+
 # $1 -> @CURRENT_SOURCE_DIR@ as replaced by meson or syntect_plugin_path, we cd into it
 # $2 -> Directory to place the finished binaries, meson replaces @OUTDIR@
 # $3 -> The final name of the gxi binary, set output: ['name'] in meson.build
 # to change it.
 # $4 -> passed to cargo --target-dir, .current_build_dir() from meson
 # $5 -> Name of the binary created by cargo inside target/
-# $6 -> if we are cross building, detected via meson .is_cross_build()
-# $7 -> Whether we should enable gtk3_22 support, enabled automatically
+# $6 -> Whether we should enable gtk3_22 support, enabled automatically
 # if we have gtk+-3.0 >= 3.22
 
 # These variables are used by gxi itself in src/globals.rs
 # to decide where to look for certain system components
-export GXI_PLUGIN_DIR="${8}"
-export GXI_LOCALEDIR="${9}"
-export GXI_VERSION="${10}"
-export GXI_XI_BINARY_PATH="${11}"
+export GXI_PLUGIN_DIR="${7}"
+export GXI_LOCALEDIR="${8}"
+export GXI_VERSION="${9}"
+export GXI_XI_BINARY_PATH="${10}"
 
 # ANSI codes for getting green colors and resetting it
 GREEN='\033[0;32m'
@@ -24,19 +25,25 @@ NO_COLOR='\033[0m'
 echo "\tGXI Plugindir: ${GREEN}${GXI_PLUGIN_DIR}${NO_COLOR}
 \tGXI Localedir:       ${GREEN}${GXI_LOCALEDIR}${NO_COLOR}
 \tGXI Version:         ${GREEN}${GXI_VERSION}${NO_COLOR}
-\tCrossbuild:          ${GREEN}${6}${NO_COLOR}
-\tDetected GTK+3.22:   ${GREEN}${7}${NO_COLOR}
+\tDetected GTK+3.22:   ${GREEN}${6}${NO_COLOR}
 "
 
 cd "$1"
-if [ "$6" = "true" ]; then
-    path="${4}/${RUST_TARGET}/release/${5}"
-else
-    path="${4}/release/${5}"
-fi
 
-if [ "$7" = "true" ]; then
+if [ "$6" = "true" ]; then
     features="--features gtk_v3_22"
 fi
 
-cargo build --target-dir "${4}" --release ${features} && cp "${path}" "${2}/${3}"
+cargo build --target-dir "${4}" --release ${features}
+
+# Cargo can place this here if we're crosscompiling
+if [ -f "${4}/${RUST_TARGET}/release/${5}" ]; then
+    path="${4}/${RUST_TARGET}/release/${5}"
+elif [ -f "${4}/release/${5}" ]; then
+    path="${4}/release/${5}"
+else
+    echo "Can't determine what dir cargo places compiled binaries in!"
+    exit 1
+fi
+
+cp "${path}" "${2}/${3}"
