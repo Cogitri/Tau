4b948fb6621b945ba16b5b7b92fa58fb04e66666
Rasmus Thomsen <oss@cogitri.dev>
2019-06-09 01:36:09 +0200

    chore(build): delete old gresource to make sure we're always using the latest version

--- src/editview/src/draw_invisible.rs
+++ src/editview/src/draw_invisible.rs
@@ -10,37 +10,38 @@ pub struct Rectangle {
     pub y: f64,
 }
 
-pub fn draw_space(cr: &Context, rect: &Rectangle) {
-    trace!("Drawing space at: {:?}", rect);
+impl Rectangle {
+    pub fn draw_space(&self, cr: &Context) {
+        trace!("Drawing space at: {:?}", self);
 
-    let x = rect.x;
-    let y = rect.y + rect.height * (2.0 / 3.0);
+        let x = self.x;
+        let y = self.y + self.height * 0.5;
 
-    let width = rect.width;
+        let width = self.width;
 
-    cr.save();
-    cr.move_to(x + width * 0.5, y);
-    cr.arc(x + width * 0.5, y, 0.8, 0.0, 2. * PI);
-    cr.stroke();
-    cr.restore();
-}
+        cr.save();
+        cr.move_to(x + width * 0.5, y);
+        cr.arc(x + width * 0.5, y, 1.0, 0.0, 2.0 * PI);
+        cr.stroke();
+        cr.restore();
+    }
 
-pub fn draw_tab(cr: &Context, rect: &Rectangle) {
-    trace!("Drawing tab at: {:?}", rect);
+    pub fn draw_tab(&self, cr: &Context) {
+        trace!("Drawing tab at: {:?}", self);
 
-    let x = rect.x;
-    let y = rect.y + rect.height * (2.0 / 3.0);
+        let x = self.x;
+        let y = self.y + self.height * 0.5;
 
-    let width = rect.width;
-    let height = rect.height;
+        let width = self.width;
+        let height = self.height;
 
-    cr.save();
-    cr.move_to(x + width * 1.0 / 8.0, y);
-    cr.move_to(x + width * 1.0 / 8.0, y);
-    cr.rel_line_to(width * 6.0 / 8.0, 0.0);
-    cr.rel_line_to(-height * 1.0 / 4.0, -height * 1.0 / 4.0);
-    cr.rel_move_to(height * 1.0 / 4.0, height * 1.0 / 4.0);
-    cr.rel_line_to(-height * 1.0 / 4.0, height * 1.0 / 4.0);
-    cr.stroke();
-    cr.restore();
+        cr.save();
+        cr.move_to(x + width * 1.0 / 8.0, y);
+        cr.rel_line_to(width * 6.0 / 8.0, 0.0);
+        cr.rel_line_to(-height * 1.0 / 4.0, -height * 1.0 / 4.0);
+        cr.rel_move_to(height * 1.0 / 4.0, height * 1.0 / 4.0);
+        cr.rel_line_to(-height * 1.0 / 4.0, height * 1.0 / 4.0);
+        cr.stroke();
+        cr.restore();
+    }
 }
