5c02f25dc3691622fb4c359a78642a42891433c3
Rasmus Thomsen <oss@cogitri.dev>
2019-06-20 16:22:44 +0200

    test(editview): add tests to editview::fonts::Font
    These tests need fontconfig and freetype2!

--- src/editview/src/fonts.rs
+++ src/editview/src/fonts.rs
@@ -4,6 +4,7 @@ use pango::{FontDescription, FontsetExt, Language};
 
 /// The `Font` Struct holds all information about the font used in the `EditView` for the editing area
 /// or the interface font (used for the linecount)
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub struct Font {
     pub font_height: f64,
     pub font_width: f64,
@@ -13,6 +14,8 @@ pub struct Font {
 }
 
 impl Font {
+    /// Create a new `Font` Struct. If `FontDescription` doesn't match any of the fonts installed on
+    /// the system Pango will choose the closest match, or the default if nothing matches.
     pub fn new(pango_ctx: &pango::Context, font_desc: FontDescription) -> Self {
         pango_ctx.set_font_description(&font_desc);
         // FIXME: Just use en-US lang here, otherwise FontMetrics may be different (as in font_ascent/
@@ -50,5 +53,70 @@ impl Font {
             font_descent,
             font_desc,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::fonts::Font;
+    use ::fontconfig::fontconfig;
+    use pango::prelude::*;
+    use pango::FontDescription;
+    use std::ffi::CString;
+
+    #[test]
+    fn test_font_metrics() {
+        unsafe {
+            let fonts_dir = CString::new("tests/assets").unwrap();
+            let ret = fontconfig::FcConfigAppFontAddDir(
+                fontconfig::FcConfigGetCurrent(),
+                fonts_dir.as_ptr() as *const u8,
+            );
+            if ret != 1 {
+                panic!("Couldn't set fontconfig dir!");
+            }
+        }
+
+        let font_map = pangocairo::FontMap::get_default().unwrap();
+        let pango_ctx = font_map.create_context().unwrap();
+
+        assert_eq!(
+            Font::new(&pango_ctx, FontDescription::from_string("Vera 12")),
+            Font {
+                font_ascent: 15.0,
+                font_descent: 4.0,
+                font_height: 19.0,
+                font_width: 9.0,
+                font_desc: FontDescription::from_string("Vera 12"),
+            }
+        )
+    }
+
+    #[test]
+    fn test_bold_font_metrics() {
+        unsafe {
+            let fonts_dir = CString::new("tests/assets").unwrap();
+            let ret = fontconfig::FcConfigAppFontAddDir(
+                fontconfig::FcConfigGetCurrent(),
+                fonts_dir.as_ptr() as *const u8,
+            );
+            if ret != 1 {
+                panic!("Couldn't set fontconfig dir!");
+            }
+        }
+
+        let font_map = pangocairo::FontMap::get_default().unwrap();
+        let pango_ctx = font_map.create_context().unwrap();
+
+        assert_eq!(
+            Font::new(&pango_ctx, FontDescription::from_string("VeraBold 12")),
+            Font {
+                font_ascent: 15.0,
+                font_descent: 4.0,
+                font_height: 19.0,
+                font_width: 9.0,
+                font_desc: FontDescription::from_string("VeraBold 12"),
+            }
+        )
     }
 }
